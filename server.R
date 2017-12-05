9c6eefb1468fc72f877d6d2d2e5da3fbc1ca983f
YutaoMa <ytma98@gmail.com>
2017-12-05 15:34:32 -0800

    :sparkles: Add 3-D view in Win Rates tab, add Top Teams tab.

--- server.R
+++ server.R
@@ -16,12 +16,28 @@ attr.colors <- c("green", "blue", "red")
 
 heroStats.results$pro_win_rate <- heroStats.results$pro_win / heroStats.results$pro_pick
 
+teams.url <- 'https://api.opendota.com/api/teams'
+teams.response <- GET(teams.url)
+teams.body <- content(teams.response, "text")
+teams.results <- fromJSON(teams.body)
+teams.tops.results <- teams.results[1:10,]
+
 shinyServer(function(input, output) {
   output$heroStats <- renderPlotly({
+    if(input$view.change == "2D") {
     p <- plot_ly(data = heroStats.results, x = ~NaN_pick, y = ~NaN_win_rate,
                  color = ~primary_attr, colors = attr.colors,
                  text = ~localized_name) %>% 
          layout(title = 'Dota 2 Heroes Win Rates vs. Picks', yaxis = list(title = "Win Rate"), xaxis = list(title = "Pick"))
+    } else {
+      p <- plot_ly(data = heroStats.results, x = ~NaN_pick, z = ~NaN_win_rate, y = ~primary_attr,
+                   color = ~primary_attr, colors = attr.colors,
+                   text = ~localized_name) %>% 
+        add_markers() %>%
+        layout(scene = list(xaxis = list(title = 'Picks'),
+                            zaxis = list(title = 'Wins'),
+                            yaxis = list(title = "Attribute")))
+    }
     return(p)
   })
   
@@ -32,6 +48,15 @@ shinyServer(function(input, output) {
                  type = "bar") %>% 
           add_trace(y = ~NaN_win_rate, name = "All Win Rates") %>% 
           layout(yaxis = list(title = "Win Rate"), barmode = "group")
+    return(p)
+  })
+  
+  output$topTeams <- renderPlotly({
+    p <- plot_ly(data = teams.tops.results,
+                 name = "Tops 10 Teams",
+                 type = "bar") %>% 
+                add_trace(x = ~tag, y = ~rating, name = "Team Rating") %>% 
+                layout(yaxis = list(title = "Rating"))
     return(p)
   })
 })
--- ui.R
+++ ui.R
@@ -6,6 +6,14 @@ library(plotly)
 library(shiny)
 
 shinyUI(navbarPage('Dota 2 Data Visualization',
-    tabPanel("Hero Win Rates", plotlyOutput('heroStats')),
-    tabPanel("Pro vs. All Win Rates", plotlyOutput('proStats'))
+    tabPanel("Hero Win Rates", 
+             sidebarLayout(
+               sidebarPanel(
+                 radioButtons("view.change", label = h3("2-D & 3-D View Changer"),
+                                              choices = list("2D" = "2D", "3D" = "3D")) 
+               ),
+               mainPanel(plotlyOutput('heroStats'))
+               )),
+    tabPanel("Pro vs. All Win Rates", plotlyOutput('proStats')),
+    tabPanel("Top 10 Teams", plotlyOutput('topTeams'))
 ))
